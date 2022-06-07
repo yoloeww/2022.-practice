74236d77407d6ec26227c4cfb67746878dbeb8a6
yolo <77111763+yoloeww@users.noreply.github.com>
2022-06-07 22:12:07 +0800

    Create 2022.6.7.kaggle2.py

---
+++ 2022.6.7.kaggle2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+train = pd.read_csv('./data/train.csv')
+test = pd.read_csv('./data/test.csv')
