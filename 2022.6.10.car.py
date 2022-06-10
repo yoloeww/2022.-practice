a003efca9f4bac2b2728b1d698463545a69041d9
yolo <77111763+yoloeww@users.noreply.github.com>
2022-06-10 22:04:00 +0800

    Create 2022.6.10.car.py

---
+++ 2022.6.10.car.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+test = pd.read_csv('test (1).csv')
+train = pd.read_csv('train (1).csv')
+test.head()
